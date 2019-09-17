7d018d1ed04ed47066a1e5e127fe7724f8a0e6a5
rajmechi <39564006+rajmechi@users.noreply.github.com>
2019-09-17 10:11:15 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,15 +5,18 @@ do
    #sleep $(echo "scale=4; $WAIT/1000" | bc)
    sleep 6
    I=$(shuf -i 1-3 -n 1)
-   D=`date -Iseconds`
+   d=`date -Iseconds`
    D=`date '+%b %d,%Y'`
    case "$I" in
-      "1") echo "$D ERROR An error is usually an exception that has been caught and not handled.
+      "1") echo "$D OPTION1 ERROR An error is usually an exception that has been caught and not handled.
        line2 test 
        line3 test 
        line4 test"
       ;;
-      "2") echo "$D OPTION2 ERROR An error is usually an exception that has been caught and not handled"
+      "2") echo "$d OPTION2 ERROR An error is usually an exception that has been caught and not handled.
+       line2 test 
+       line3 test 
+       line4 test"
       ;;
       "3") echo "$D OPTION3 ERROR An error is usually an exception that has been caught and not handled."
       ;;
