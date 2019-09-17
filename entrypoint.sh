d73a786d3bb431a2cc47549cec4925de3c1298d2
rajmechi <39564006+rajmechi@users.noreply.github.com>
2019-09-17 09:55:26 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,10 @@ do
    D=`date -Iseconds`
    D=`date '+%b %d,%Y'`
    case "$I" in
-      "1") echo "$D ERROR An error is usually an exception that has been caught and not handled."
+      "1") echo "\"ERROR An error is usually an exception that has been caught and not handled.
+       line2 test
+       line3 test
+       line4 test \""
       ;;
       "2") echo "$D {
   "glossary": {
