4d28574732aa005dc9ecb72d0c8a451e6a64d384
rajmechi <39564006+rajmechi@users.noreply.github.com>
2019-09-16 23:40:33 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 while [ 1 ]
 do
-   WAIT=$(shuf -i $1-$2 -n 1)
-   sleep $(echo "scale=4; $WAIT/1000" | bc)
+   #WAIT=$(shuf -i $1-$2 -n 1)
+   #sleep $(echo "scale=4; $WAIT/1000" | bc)
+   sleep 20
    I=$(shuf -i 1-4 -n 1)
    D=`date -Iseconds`
    case "$I" in
       "1") echo "$D ERROR An error is usually an exception that has been caught and not handled."
       ;;
-      "2") echo "{
+      "2") echo "$D {
   "glossary": {
     "title": "example glossary",
     "GlossDiv": {
