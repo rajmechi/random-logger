e33d487af4a8ad5803f58e2b5e95e900aee6891d
rajmechi <39564006+rajmechi@users.noreply.github.com>
2020-01-14 15:43:25 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,9 +8,9 @@ do
    d=`date -Iseconds`
    D=`date '+%b %d,%Y'`
    case "$I" in
-      "1") echo "{$D OPTION1 ERROR An error is usually an exception that has been caught and not handled. case 1}"
+      "1") echo "{$D OPTION1 ERROR An error is usually an exception that has been caught and not handledThe kubelet uses liveness probes to know when to restart a container. Liveness probes can fix a situation in which an application is running but unable to make progress, for example. Restarting a container in such a state can help to make the application available despite bugs The kubelet uses liveness probes to know when to restart a container. Liveness probes can fix a situation in which an application is running but unable to make progress, for example. Restarting a container in such a state can help to make the application available despite bugs... case 1}"
       ;;
-      "2") echo "{$d OPTION2 ERROR An error is usually an exception that has been caught and not handled. case 2}"
+      "2") echo "{$d OPTION2 ERROR An error is usually an exception that has been caught and not handled The kubelet uses liveness probes to know when to restart a container. Liveness probes can fix a situation in which an application is running but unable to make progress, for example. Restarting a container in such a state can help to make the application available despite bugs. The kubelet uses liveness probes to know when to restart a container. Liveness probes can fix a situation in which an application is running but unable to make progress, for example. Restarting a container in such a state can help to make the application available despite bugs.. case 2}"
       ;;
       "3") echo "$D OPTION3 ERROR An error is usually an exception that has been caught and not handled. case 3"
       ;;
